f9e1a12b20a812a5345c6ffa5168c7055835e4af
ItzJvs <Jagtapvs9832@gmail.com>
2022-09-14 11:43:56 +0530

    project completed..

--- src/Pomodro.js
+++ src/Pomodro.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import GoogleLogin, { GoogleLogout } from "react-google-login";
 
 class Pomodro extends Component {
   constructor() {
@@ -10,6 +11,8 @@ class Pomodro extends Component {
       isBreak: false,
       isPaused: false,
       msg: "",
+      isLoggedIn: false,
+      user: null,
       clock: () => {
         if (!this.state.isBreak) {
           if (this.state.minutes >= 0 && this.state.seconds >= 1) {
@@ -25,7 +28,6 @@ class Pomodro extends Component {
         } else {
           if (this.state.minutes >= 0 && this.state.seconds >= 1) {
             if (this.state.seconds === 0) {
-              let minutes = this.state.minutes;
               this.setState({ minutes: this.state.minutes - 1 });
               this.setState({ seconds: 60 });
             } else {
@@ -44,8 +46,53 @@ class Pomodro extends Component {
       },
       interval: () => {},
     };
+    this.tryLogout = this.tryLogout.bind(this);
   }
+  componentDidMount() {
+    this.checkUserState();
+  }
+  tryLogin = (user) => {
+    let userInfo = user.getBasicProfile();
+    let userFirstName = userInfo.getGivenName();
+    let userName = userInfo.getName();
+    let userEmail = userInfo.getEmail();
+    let userPic = userInfo.getImageUrl();
 
+    let userToken = user.tokenObj.access_token.toString();
+
+    let value = {
+      userName: userFirstName,
+      userFullName: userName,
+      userEmail: userEmail,
+      userPic: userPic,
+      userToken: userToken,
+    };
+    localStorage.setItem("userInfo", JSON.stringify(value));
+    this.checkUserState();
+  };
+  tryLogout() {
+    debugger;
+    localStorage.removeItem("userInfo");
+    this.setState({
+      ...this.state,
+      isLoggedIn: false,
+      user: {},
+    });
+    this.checkUserState();
+  }
+  checkUserState() {
+    let userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    if (userInfo == null) {
+      this.state.isLoggedIn = false;
+      this.setState({
+        ...this.state,
+      });
+    } else {
+      this.state.isLoggedIn = true;
+      this.state.user = userInfo;
+      this.setState({ ...this.state });
+    }
+  }
   reset(isTimeOut) {
     clearInterval(this.state.interval);
     this.setState({
@@ -60,12 +107,27 @@ class Pomodro extends Component {
     });
   }
   render() {
+    const clientId = "1082757406270-hjptn6fkt7o21tj9cl3r6n2qpqangpro.apps.googleusercontent.com";
     return (
       <div className="pomodro">
+        {this.state.isLoggedIn ? (
+          <>
+            Hi..{this.state.user.userName}
+            <br />
+          </>
+        ) : (
+          <GoogleLogin className="GoogleLogin" clientId={clientId} buttonText="Login" onSuccess={this.tryLogin} cookiePolicy={"single_host_origin"} />
+        )}
+        {this.state.isLoggedIn ? (
+          <>
+            <br />
+            <GoogleLogout className="GoogleLogout" clientId={clientId} buttonText="LogOut" onLogoutSuccess={this.tryLogout} />
+          </>
+        ) : null}
         <span className="time">
           {this.state.minutes} Min {this.state.seconds} Sec
           <br />
-          {this.state.isBreak == true ? (
+          {this.state.isBreak === true ? (
             <>
               <span className="breakTitle"> {this.state.msg} </span>
             </>
@@ -80,6 +142,7 @@ class Pomodro extends Component {
             value="Start"
             onClick={() => {
               this.setState({ isRunning: true, msg: "" });
+              clearInterval(this.state.interval);
               this.state.interval = setInterval(() => {
                 this.state.clock();
               }, 1000);
